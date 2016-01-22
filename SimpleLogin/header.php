fb5f1a9fca2645a905363e6467914982f8215b3b
Pranjal Nadhani <Pranjal Nadhani@CodeZombie>
2016-01-23 00:39:15 +0530

    Initial commit

---
+++ SimpleLogin/header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Login Form in PHP with Session</title>
+
+    <!-- Shortcut icon -->
+    <link rel="shortcut icon" href="favicon.gif" type="icon/x-icon" />
+    
+    <!-- Stylesheets -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <link href="css/site.css" rel="stylesheet" type="text/css"/>
+
+    <!-- Javascripts -->
+    <script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+
+</head>
